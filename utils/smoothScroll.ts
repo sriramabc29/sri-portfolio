95bfe04aed04b9b276a1896ce14a851e778c64d1
sriramabc29 <sriramabc29@gmail.com>
2025-02-08 18:34:45 +0000

    Add files via upload

---
+++ utils/smoothScroll.ts
@@ -0,0 +1,10 @@
+export const smoothScroll = (elementId: string) => {
+  const element = document.getElementById(elementId)
+  if (element) {
+    element.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    })
+  }
+}
+
